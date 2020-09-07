1f0567bb3ebaf1dda99a1bf57c6f02190284f4c9
David Napadensky <davidnapadensky@gmail.com>
2020-09-07 14:10:16 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,214 @@
+<template>
+  <v-app>
+    <v-app-bar app class="white" z-index="1000" height="54px" elevation='1'>
+      <span class="bnegra"></span>
+      <div>
+        <v-img
+          alt="Around Logo"
+          class
+          contain
+          src="./assets/Logo/Black.png"
+          transition="scale-transition"
+          width="92"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <nav class="menu_nav">
+        <ul class="menu_items">
+          <!-- <li v-for="(item,i) in items" :key="i" class="mr-10">{{item.title}}</li> -->
+
+          <li class="mr-10" @click="loguear">Log In</li>
+          <li class="mr-10">Community</li>
+          <li class="mr-10 px-5 pys">Publish Your Space</li>
+        </ul>
+      </nav>
+
+      <div class="menu hidden-sm-and-up">
+        <input type="checkbox" id="check-menu" @click=" showMenu " />
+        <label for="check-menu">
+          <div class="icono-menu">
+            <div class="barra-menu"></div>
+            <div class="barra-menu"></div>
+            <div class="barra-menu"></div>
+          </div>
+        </label>
+      </div>
+    </v-app-bar>
+
+    <v-dialog v-model="logIn" max-width="414px" persistent>
+      <v-card elevation="20" :class="`rounded-xl`" class="align-center d-flex flex-column pa-5">
+        <v-row class="align-self-end">
+          <v-btn @click="logIn = !logIn" color="gris" rounded text fab>
+            <strong>X</strong>
+          </v-btn>
+        </v-row>
+        <v-card-title>Log In</v-card-title>
+        <v-card-actions class="d-flex flex-column">
+          <v-btn rounded color="socialLog">
+            <v-icon left class="black">mdi-light mdi-18px mdi-linkedin</v-icon>continue with LinkediN
+          </v-btn>
+          <v-btn rounded color="socialLog" class="my-5 btn-same">
+            <v-icon left class>mdi-18px mdi-email</v-icon>
+            <span class="mr-8">continue with Email</span>
+          </v-btn>
+        </v-card-actions>
+        <v-card-text class="text-center">
+          <small>
+            By entering, I accept Around’s Terms &
+            <br />Conditions of Use and Privacy Policy
+          </small>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    drawer: false,
+    logIn: false,
+    items: [
+      { title: "Log In" },
+      { title: "Community" },
+      { title: "Publish Your Space" },
+    ],
+  }),
+  methods: {
+    showMenu() {
+      const menu_items = document.querySelector(".menu_items");
+      menu_items.classList.toggle("show");
+    },
+    loguear() {
+      if (this.drawer === false) {
+        const menu_items = document.querySelector(".menu_items");
+        const che = document.getElementById("check-menu");
+        menu_items.classList.remove("show");
+        che.checked = false;
+        console.log("entro");
+      }
+      this.logIn = !this.logIn;
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+
+.bnegra {
+  background-color: black;
+  width: 26px;
+  height: 26px;
+  border-radius: 100%;
+  margin-right: 8px;
+  margin-top: 5px;
+}
+.menu_nav {
+  @media screen and (max-width: 600px) {
+    height: 54px;
+  }
+}
+.menu_items {
+  display: flex;
+  list-style: none;
+  margin-right: 30px;
+  cursor: pointer;
+  li {
+    border-radius: 25px;
+    margin: 0 5px;
+    @media screen and (max-width: 600px) {
+      line-height: 50px;
+      margin-top: 10px;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    position: fixed;
+    width: 100%;
+    height: calc(100% - 54px);
+    background-color: #fff;
+    top: -100vh;
+    transition: all 0.8s;
+    flex-direction: column;
+    // align-self: start;
+  }
+  .pys {
+     border: 1px solid black;
+    border-radius: 30px;
+    @media screen and (max-width: 600px) {
+    border: 1px solid black;
+    border-radius: 30px;
+    display: inline-block;
+    width: 200px;
+    background-color: #000;
+    color: white;
+    padding: 1px 2px;
+  }}
+}
+ul {
+  left: 0;
+}
+ul.show {
+  @media screen and (max-width: 600px) {
+    top: 54px;
+    right: -30px;
+    background-color: #fff;
+    width: 100%;
+    height: 100vh;
+  }
+}
+
+// menu hamburguesa animado
+.menu {
+  padding: 10px;
+  width: 36px;
+  margin-right: 15px;
+  margin-top: 7px;
+
+  height: 36px;
+}
+.icono-menu {
+  width: 24px;
+  cursor: pointer;
+  overflow: hidden;
+  height: 24px;
+}
+.barra-menu {
+  width: 100%;
+  height: 2.5px;
+  border-radius: 0px;
+  transition: 0.5s;
+  background: #000;
+}
+.barra-menu:nth-child(1),
+.barra-menu:nth-child(2) {
+  margin-bottom: 3px;
+}
+
+#check-menu {
+  display: none;
+}
+#check-menu:checked + label .main-content .main-menu {
+  opacity: 1;
+  height: auto;
+}
+#check-menu:checked + label .icono-menu .barra-menu {
+  margin: 0;
+}
+#check-menu:checked + label .icono-menu .barra-menu:nth-child(1) {
+  transform: rotate(-45deg) translate(-5px, 6.69px);
+}
+#check-menu:checked + label .icono-menu .barra-menu:nth-child(2) {
+  transform: translate(-50px);
+}
+#check-menu:checked + label .icono-menu .barra-menu:nth-child(3) {
+  transform: rotate(45deg) translate(3px, 1.5px);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,146 @@
+<template >
+  <v-container fluid class="px-0 overflow-hidden pt-0">
+    <v-card class="rounded-b-xl" v-resize="onResize">
+      <v-row justify="center">
+        <v-col cols="10" lg="6" :class=" {'pl-6':!mob, 'pb-0':!mob, } ">
+          <v-row justify="center">
+            <v-img
+              max-height=" 880 "
+              min-width="280"
+              src="../assets/header_hero.png"
+              :class=" {'rounded-bl-xl':!mob, 'rounded-t-xl':mob, } "
+            ></v-img>
+          </v-row>
+        </v-col>
+        <v-col cols="12" lg="6" align-self="center">
+          <v-row justify="center">
+            <div>
+              <v-banner sticky light class="white pa-0">
+                <template v-slot:actions light>
+                  <input type="text" placeholder="Find your Workspace" />
+                  <v-icon>mdi-magnify</v-icon>
+                </template>
+              </v-banner>
+              <v-card-actions class="d-flex"></v-card-actions>
+              <v-card-title primary-title>
+                <h2 :class="{'display-3':!mob, 'Founders-Approved-Wo':!mob}">
+                  Founders Apporved
+                  <br />Workspaces
+                </h2>
+              </v-card-title>
+              <v-card-text>
+                <p
+                  :class="{'display-1':!mob, 'Unique-Workspaces-fo':!mob}"
+                >Unique Workspaces for growing teams</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="gray ml-5" rounded text>
+                  E
+                  <span class="text-lowercase">xplore</span>
+                </v-btn>
+              </v-card-actions>
+            </div>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
+    <!-- Titulo__Logos -->
+    <v-row justify="center" class="mt-15">
+      <v-col cols="12">
+        <h2 class="text-center">We've been Around</h2>
+        <!-- Img__Logos -->
+        <v-row justify="center" class="mt-5">
+          <v-col cols="auto" lg="3" class="pa-0" align-self="center">
+            <img
+              class="ma-auto"
+              src="../assets/Logo/4-expansion-logo-medio-oficial_2020-09-06/4-expansion-logo-medio-oficial.png"
+              alt
+            />
+          </v-col>
+          <v-col cols="auto" lg="3" class="pa-0" align-self="center">
+            <img
+              src="../assets/Logo/1413842518-entrepreneur-logo_2020-09-06/1413842518-entrepreneur-logo.png"
+              alt
+            />
+          </v-col>
+          <v-col cols="auto" lg="3" class="pa-0" align-self="center">
+            <img src="../assets/Logo/bitmap_2020-09-06/bitmap.png" alt />
+          </v-col>
+
+          <v-col cols="auto" lg="3" class="pa-0" align-self="center">
+            <img src="../assets/Logo/bitmap_2020-09-06 (1)/bitmap.png" alt />
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati autem ullam, consequuntur laudantium culpa officiis deleniti at molestiae? Exercitationem accusantium officia mollitia sapiente odit animi error nobis fugiat ducimus. Similique.
+      Dolore deleniti, excepturi quo explicabo, quae nostrum ad distinctio suscipit aut veniam maxime corrupti doloribus perspiciatis cupiditate reiciendis sed commodi ipsum ducimus vel laudantium assumenda ipsam quos repellat. Nisi, inventore!
+      Tenetur nam voluptatum similique rerum, sapiente harum quos velit impedit consectetur. Culpa dolores reprehenderit accusamus mollitia inventore maxime repudiandae qui est esse repellat eaque, dignissimos soluta sint accusantium ea fuga.
+      Soluta, illo. Ullam nam corporis rem, modi voluptatum eius, incidunt odit facere eaque, ab minima praesentium. Cupiditate, laborum. Quis rem iure hic eveniet incidunt eligendi velit odio tenetur optio repudiandae.
+      Odit nulla tempore incidunt veritatis, optio itaque quam delectus possimus est quo alias libero, soluta nisi. Ullam quibusdam facere fugit totam quo rerum aspernatur nisi, amet rem ex quasi mollitia.
+    </p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  data() {
+    return {
+      mob: true,
+      windowSize: {
+        x: 0,
+        y: 0,
+      },
+    };
+  },
+  methods: {
+    onResize() {
+      this.windowSize = { x: window.innerWidth, y: window.innerHeight };
+      this.windowSize.x < 1270 ? (this.mob = true) : (this.mob = false);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.Founders-Approved-Wo {
+  @media (min-width: 700px) {
+    font-family: HKGrotesk;
+    font-size: 3.5rem;
+    font-weight: 500;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.06;
+    letter-spacing: -1.13px;
+    color: #000000;
+  }
+}
+.Unique-Workspaces-fo {
+  @media (min-width: 700px) {
+    max-width: 501px;
+    height: 99px;
+    font-family: HKGrotesk;
+    font-size: 32px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.34;
+    letter-spacing: -0.67px;
+    color: #000000;
+  }
+}
+img {
+  @media (min-width: 995px) {
+    min-width: 250px;
+  }
+  @media (min-width: 600px) {
+    width: 150px;
+  }
+  @media (min-width: 100px) {
+    max-width: 250px;
+  }
+}
+</style>
